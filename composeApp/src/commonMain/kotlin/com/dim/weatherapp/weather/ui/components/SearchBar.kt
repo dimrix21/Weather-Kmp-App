3a2562c6912d48d75d6325ace87fe5a7d7d6bd70
Hezi <>
2025-09-14 21:21:54 +0300

    fixed search bar issues

--- composeApp/src/commonMain/kotlin/com/dim/weatherapp/weather/ui/components/SearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/dim/weatherapp/weather/ui/components/SearchBar.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -16,6 +18,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.dim.weatherapp.core.presentation.theme.WeatherAppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -25,12 +31,20 @@ fun SearchBar(
     cityName: String,
     onSearchClicked: (String) -> Unit,
 ) {
+    var textFieldValue by remember(cityName) {
+        mutableStateOf(
+            TextFieldValue(
+                text = cityName,
+                selection = TextRange(cityName.length) // cursor בסוף
+            )
+        )
+    }
 
-    var currentValue by remember(cityName) { mutableStateOf(cityName) }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     OutlinedTextField(
-        value = currentValue,
-        onValueChange = { currentValue = it },
+        value = textFieldValue,
+        onValueChange = { textFieldValue = it },
         placeholder = { Text("Enter a city name", color = Color(0xFFB0B0B3)) },
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(8.dp),
@@ -38,19 +52,29 @@ fun SearchBar(
             focusedContainerColor = Color.White,
             unfocusedContainerColor = Color.White
         ),
-        singleLine = true
+        singleLine = true,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                keyboardController?.hide()
+                onSearchClicked(textFieldValue.text)
+            }
+        )
     )
 
     Spacer(modifier = Modifier.height(8.dp))
 
     Button(
-        onClick = { onSearchClicked(currentValue) },
+        onClick = {
+            keyboardController?.hide()
+            onSearchClicked(textFieldValue.text)
+        },
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(8.dp)
-
     ) {
         Spacer(modifier = Modifier.width(4.dp))
-
         Text("Search")
     }
 }
--- composeApp/src/commonMain/kotlin/com/dim/weatherapp/weather/ui/presenters/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/dim/weatherapp/weather/ui/presenters/MainScreen.kt
@@ -9,10 +9,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.dim.weatherapp.core.presentation.theme.TopBarBackground
@@ -23,17 +19,6 @@ import com.dim.weatherapp.weather.ui.presenters.weather_screen.WeatherScreenRoot
 fun MainScreen(
     modifier: Modifier = Modifier
 ) {
-    var cityName by remember { mutableStateOf("Tokyo") } // Example city, can be dynamic
-    val recentSearches =
-        listOf("Tokyo", "Rome", "Israel", "Las Vegas", "Tel Aviv") // Sample data
-    // Placeholder for actual weather data - you'll fetch this based on cityName
-    val temperature = "26°C"
-    val condition = "Broken Clouds"
-    val humidity = "78%"
-    val windSpeed = "5.66 m/s"
-
-
-
     Scaffold(
         modifier = modifier,
         topBar = {
